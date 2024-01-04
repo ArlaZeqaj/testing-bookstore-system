cc8c6e19f3e523f0a5092ac1f7b23213a414c876
estela-mele <estelaxmele@gmail.com>
2024-01-04 11:49:06 +0100

    fixed librarian class error

--- src/model/Librarian.java
+++ src/model/Librarian.java
@@ -1,8 +1,10 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Librarian extends Employee {
-    public Librarian(String name, String surname, String birthDate, String phoneNumber, int email, AccessLevel salary) {
-        super(name, surname, birthDate, phoneNumber, email, salary, AccessLevel.SIMPLE);
+    public Librarian(String name, String surname, LocalDate birthDate, String phoneNumber, double salary) {
+        super(name, surname, birthDate, phoneNumber, salary, AccessLevel.SIMPLE);
     }
 
     @Override
